74ecabbe61bdb501579761e1f525d7a262bdf251
HamdiNur <hamdoonur26@gmail.com>
2025-06-29 11:21:36 +0300

    Add backend and Flutter app  register and login

---
+++ e_learning_app/lib/widgets/custom_text_field.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String hintText;
+  final IconData icon;
+  final bool isPassword;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+
+  const CustomTextField({
+    Key? key,
+    required this.hintText,
+    required this.icon,
+    required this.controller,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+  }) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: const Color(0xFFE8E8E8),
+          width: 1.5,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.04),
+            blurRadius: 8,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: widget.controller,
+        obscureText: widget.isPassword ? _obscureText : false,
+        keyboardType: widget.keyboardType,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: Color(0xFF2D2D2D),
+        ),
+        decoration: InputDecoration(
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Icon(
+              widget.icon,
+              color: const Color(0xFF8B8B8B),
+              size: 22,
+            ),
+          ),
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                    color: const Color(0xFF8B8B8B),
+                    size: 22,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                )
+              : null,
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Color(0xFFB8B8B8),
+          ),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
